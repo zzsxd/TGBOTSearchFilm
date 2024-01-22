41c56af78e628a8b44ae32d170f4c858a989e32c
lilfuz <lilfuz@gmail.com>
2024-01-22 19:00:31 +0300

    Code refactoring

--- Bot.py
+++ Bot.py
@@ -4,32 +4,30 @@ from telebot import types
 bot = telebot.TeleBot('6723388582:AAFgzZfo9KG-UE8ZDKkxsyylwLJMAkEXms4')
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'creators'])
 def start(message):
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    bot.reply_to(message, 'Привет👋\nЯ SearchFilmBot🤖 - помогу с поиском интересного фильма!\nНапишите /creators для получения информации о создателях.')
-    btn1 = types.InlineKeyboardButton('Жанры', callback_data='genre')
-    btn2 = types.InlineKeyboardButton('Года', callback_data='years')
-    btn3 = types.InlineKeyboardButton('Поиск по названию', callback_data='name')
-    markup.add(btn1, btn2, btn3)
-    bot.send_message(message.chat.id, 'Выберите действие✅', reply_markup=markup)
-
-
-@bot.message_handler(commands=['creators'])
-def creators(message):
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    bot.reply_to(message, 'Создатели:\nzzsxd - фронтенд составляющая бота.\nSBR - бэкенд составляющая бота.')
-    btn1 = types.InlineKeyboardButton('Жанры', callback_data='genre')
-    btn2 = types.InlineKeyboardButton('Года', callback_data='years')
-    btn3 = types.InlineKeyboardButton('Поиск по названию', callback_data='name')
-    markup.add(btn1, btn2, btn3)
-    bot.send_message(message.chat.id, 'Выберите действие✅', reply_markup=markup)
+    command = message.text.replace('/', '')
+    if command == 'start':
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        bot.reply_to(message, 'Привет👋\nЯ SearchFilmBot🤖 - помогу с поиском интересного фильма!\nНапишите /creators для получения информации о создателях.')
+        btn1 = types.InlineKeyboardButton('Жанры', callback_data='genre')
+        btn2 = types.InlineKeyboardButton('Года', callback_data='years')
+        btn3 = types.InlineKeyboardButton('Поиск по названию', callback_data='name')
+        markup.add(btn1, btn2, btn3)
+        bot.send_message(message.chat.id, 'Выберите действие✅', reply_markup=markup)
+    elif command == 'creators':
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        bot.reply_to(message, 'Создатели:\nzzsxd - фронтенд составляющая бота.\nSBR - бэкенд составляющая бота.')
+        btn1 = types.InlineKeyboardButton('Жанры', callback_data='genre')
+        btn2 = types.InlineKeyboardButton('Года', callback_data='years')
+        btn3 = types.InlineKeyboardButton('Поиск по названию', callback_data='name')
+        markup.add(btn1, btn2, btn3)
+        bot.send_message(message.chat.id, 'Выберите действие✅', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['photo', 'video', 'voice', 'audio', 'image', 'sticker'])
 def error(message):
     bot.reply_to(message, '🚫Ошибка: неверный формат ввода🚫')
-
 
 
 @bot.callback_query_handler(func=lambda call: True)
